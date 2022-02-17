0c0a82e1d7905a0a3aec154838b58eb6c39e43b5
hoogi91 <hoogi20@googlemail.com>
2022-02-17 21:54:22 +0100

    Add missing background styling configuration

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,8 @@
 <?php
+
 defined('TYPO3') or die();
 
-(static function () {
+(static function (string $extKey = 'charts') {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_charts_domain_model_chartdata');
+    $GLOBALS['TBE_STYLES']['skins'][$extKey]['stylesheetDirectories'][] = 'EXT:' . $extKey . '/Resources/Public/Css/Backend/';
 })();
